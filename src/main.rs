78e82fc6a4db3dc6bccc1ea4826b9e70c9d9d5d5
Fenhl <fenhl@fenhl.net>
2024-08-18 15:27:19 +0000

    Move lints to Cargo.toml; Reformat dependencies for `cargo add` compat; Use serde_json_path_to_error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-#![deny(rust_2018_idioms, unused, unused_crate_dependencies, unused_import_braces, unused_lifetimes, unused_qualifications, warnings)]
-#![forbid(unsafe_code)]
-
 use {
     std::{
         collections::{
